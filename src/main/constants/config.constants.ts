b4ef2e63ee29d951ca72ae76041aa833a5ca1b49
liqms <liqms@msn.cn>
2026-09-07 09:00:48 +0800

    修改版本号

--- src/main/constants/config.constants.ts
+++ src/main/constants/config.constants.ts
@@ -32,7 +32,7 @@ export const DEFAULT_APP_CONFIG: AppConfig = {
       profession: PROFESSION.PM,
     },
   },
-  version: "0.1.6",
+  version: "0.1.7",
   workspace: "",
   currentProjectId: "",
   isFirstLaunch: true,
