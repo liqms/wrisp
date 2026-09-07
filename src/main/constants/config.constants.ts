7694f4ee469251aecf529abd46993c9b31772cdf
liqms <liqms@msn.cn>
2026-09-07 17:17:25 +0800

    修改版本号

--- src/main/constants/config.constants.ts
+++ src/main/constants/config.constants.ts
@@ -32,7 +32,7 @@ export const DEFAULT_APP_CONFIG: AppConfig = {
       profession: PROFESSION.PM,
     },
   },
-  version: "0.1.8",
+  version: "0.1.9",
   workspace: "",
   currentProjectId: "",
   isFirstLaunch: true,
--- src/main/constants/model.constants.ts
+++ src/main/constants/model.constants.ts
@@ -9,7 +9,7 @@ export const DEFAULT_MODEL_CONFIG: ModelConfig = {
     providerPriority: [],
     enableAiMode: false,
     enableCloudAi: false,
-    version: "0.1.8",
+    version: "0.1.9",
     updatedAt: TimeUtil.toISOString(Date.now()),
 }
 
